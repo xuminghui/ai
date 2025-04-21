3dd8bda172f800cb3c3722130df8d0b7c536094e
xuminghui <xuminghui18610740807@gmail.com>
2025-04-21 17:29:02 +0800

    update

--- embedings/faiss_embedding.py
+++ embedings/faiss_embedding.py
@@ -1,7 +1,7 @@
 #使用zhipuai的embedding
 from langchain_community.embeddings import ZhipuAIEmbeddings
 #加载器
-from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loaders import DirectoryLoader
 #分割器
 from langchain_text_splitters import CharacterTextSplitter
 
@@ -11,7 +11,8 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-loader = TextLoader("embedings/demo.txt")
+# 加载多个文件
+loader = DirectoryLoader("embedings", glob="*.txt")
 document=loader.load()
 text_splitter = CharacterTextSplitter(chunk_size=100, chunk_overlap=0)
 docs = text_splitter.split_documents(document)
@@ -20,6 +21,10 @@ embeddings = ZhipuAIEmbeddings(model="embedding-3",api_key=os.getenv("ZHIPU_API_
 #创建向量数据库
 vector_store = FAISS.from_documents(docs, embeddings)
 #查询
-query = "人工智能"
-docs = vector_store.similarity_search(query)
-print(docs[0].page_content)
+query = "老徐的妻子是谁？"
+docs_with_scores= vector_store.similarity_search_with_relevance_scores(query)
+for doc, score in docs_with_scores:
+    print("Score: ", score)
+    print(doc.page_content)
+    print("metadata: ",doc.metadata)
+
